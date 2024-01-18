1ae5a5c4cd04d62132d8647cdb21a3b15b5b9447
Nathan Rydel <nathanrydel@gmail.com>
2024-01-18 14:43:18 -0700

    updated imports fixed Component naming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import BoxList from "./BoxList"
 
 function App() {
   return (
     <div className="App">
-      <Boxlist />
+      <BoxList />
     </div>
   );
 }

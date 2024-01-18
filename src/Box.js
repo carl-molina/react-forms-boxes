670eb084421d27a32fde7512a208bd324317b743
Carl Molina <carlandrew.molina@gmail.com>
2024-01-18 12:16:38 -0800

    Finished first draft of BoxList component

---
+++ src/Box.js
@@ -0,0 +1,16 @@
+
+
+
+function Box({ id, width, height, backgroundColor, remove }) {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+
+
+export default Box;
---
+++ src/BoxList.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Box from "./Box";
+import { v4 as uuid } from "uuid";
+
+
+function BoxList() {
+  const [boxes, setBoxes] = useState([]);
+
+  function addBox(box) {
+    const newBox = {...box, id: uuid() };
+    setBoxes(curr => [...curr, newBox]);
+  }
+
+  function removeBox(id) {
+    setBoxes(curr => curr.filter(x => x.id !== id));
+  }
+
+  return (
+    <div>
+      <NewBoxForm
+        add={addBox}/>
+      {/* add boxes.map here */}
+
+      {boxes.map(({id, width, height, backgroundColor}) =>
+        (<Box
+          key={id}
+          id={id}
+          width={width}
+          height={height}
+          backgroundColor={backgroundColor}
+          remove={removeBox}
+        />))};
+
+    </div>
+  )
+}
+
+
+
+export default BoxList;

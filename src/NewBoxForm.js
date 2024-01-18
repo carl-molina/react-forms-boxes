c028f74ced235f0931049166b76c78bfde361c85
Nathan Rydel <nathanrydel@gmail.com>
2024-01-18 13:40:38 -0700

    first pass at NewBoxForm comp

---
+++ src/NewBoxForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { v4 as uuid } from "uuid";
+
+/**
+ * A controlled form to create new a Box in BoxList
+ *
+ * Props:
+ * - add: function - creates a new Box
+ *
+ * State:
+ * - none
+ *
+ * App -> BoxList -> NewBoxForm
+ */
+
+function NewBoxForm({ add }) {
+  const [formData, setFormData] = useState({
+    height: "",
+    width: "",
+    backgroundColor: "",
+  });
+
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    add({ ...formData, id: uuid() });
+    setFormData({ height: "", width: "", backgroundColor: "" });
+  }
+
+  return (
+    <div className="NewFormBox">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="NewFormBox-height">Height: </label>
+          <input
+            id="NewBoxForm-height"
+            onChange={handleChange}
+            name="height"
+            value={formData.height}
+          />
+        </div>
+        <div>
+          <label htmlFor="NewFormBox-width">Width: </label>
+          <input
+            id="NewBoxForm-width"
+            onChange={handleChange}
+            name="width"
+            value={formData.width}
+          />
+        </div>
+        <div>
+          <label htmlFor="NewFormBox-backgroundColor">Background Color: </label>
+          <input
+            id="NewBoxForm-backgroundColor"
+            onChange={handleChange}
+            name="backgroundColor"
+            value={formData.backgroundColor}
+          />
+        </div>
+        <button className="NewFormBox-submitBtn">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewBoxForm;
